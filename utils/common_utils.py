38e474ecfb6bdfbda7e6bc2f2000c344043848b2
sn0wbars <alex-borz@yandex.ru>
2018-05-21 19:05:50 +0300

    noNeedForGrid

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -75,15 +75,13 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation=None):
     images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
 
     grid = get_image_grid(images_np, nrow)
-    plt.grid(False)
     
     plt.figure(figsize=(len(images_np)+factor,12+factor))
     if images_np[0].shape[0] == 1:
-        plt.grid(False)
         plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
     else:
-        plt.grid(False)
         plt.imshow(grid.transpose(1,2,0), interpolation=interpolation)
+    plt.grid(False)
     plt.show()
     
     return grid
